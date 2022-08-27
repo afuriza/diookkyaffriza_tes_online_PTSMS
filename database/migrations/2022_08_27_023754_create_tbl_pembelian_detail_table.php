b0bf96e56daa046d7d4e9e711c5cea547578771f
DESKTOP-FAS9NCD\afuyan <dioaffriza@gmail.com>
2022-08-27 10:29:06 +0700

    no message

--- database/migrations/2022_08_27_023754_create_tbl_pembelian_detail_table.php
+++ database/migrations/2022_08_27_023754_create_tbl_pembelian_detail_table.php
@@ -15,12 +15,12 @@ return new class extends Migration
     {
         Schema::create('tbl_pembelian_detail', function (Blueprint $table) {
             $table->id();
-            $table->integer('id_pembelian');
-            $table->foreign('id_pembelian')->references('id')->on('tbl_pembelian')->onDelete('cascade');
-            $table->string('kode_barang', 255);
-            $table->foreign('kode_barang')->references('kode_barang')->on('tbl_barang')->onDelete('cascade');
-            $table->integer('qty', 11);
-            $table->decimal('harga', 10);
+            $table->integer('id_pembelian')->unsigned()->index()->nullable();
+            $table->foreign('id_pembelian')->references('id')->on('tbl_pembelian')->onDelete('set null');
+            // $table->string('kode_barang')->nullable();
+            // $table->foreign('kode_barang')->references('kode_barang')->on('tbl_barang')->onDelete('cascade');
+            $table->integer('qty');
+            $table->decimal('harga');
             $table->timestamps();
         });
     }
